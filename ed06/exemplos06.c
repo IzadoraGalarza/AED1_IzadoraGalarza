8e375a10aa3a03429f6f26945d4546c527e173b0
Izadora Galarza Alves <86213588+IzadoraGalarza@users.noreply.github.com>
2025-04-02 23:29:00 -0300

    att ed06

--- ed06/exemplos06.c
+++ ed06/exemplos06.c
@@ -121,6 +121,8 @@ Method_05a - Mostrar certa quantidade de valores recursivamente.
    @param x - quantidade de valores a serem mostrados 
  */
 
+ 
+
  void method_05a (int x)
  {
     if (x>1)
@@ -146,29 +148,200 @@ void method_05 ()
    @param x - quantidade de valores a serem mostrados
  */
 
-void method_06 ()
+double somarFracoes (int x)
 {
-    
+	double soma=0.0;
+	if(x>1)
+	{
+		soma = (2.0*(x-1))/(2.0*(x-1)+1) + somarFracoes (x-1);
+		IO_printf ( "%d: %.4lf/%.4lf\n", x, (2.0*(x-1)), (2.0*(x-1)+1) );
+	}
+	else
+	{
+		soma=1.0;
+		IO_printf("%d: %lf\n",x,1.0);
+	}
+	return(soma);
 }
 
+void method_06 ()
+{
+	double soma= 0.0;
+	IO_id("Method_06 - exemplo0606");
+	soma = somarFracoes(5);
+	IO_printf("soma = %lf\n", soma);
+	IO_pause("Apertar ENTER para terminar");
+}
+
+/** saida : 1: 1.000000
+2: 2.0000/3.0000
+3: 4.0000/5.0000
+4: 6.0000/7.0000
+5: 8.0000/9.0000
+soma = 4.212698 */
+
+/**
+Acrescentar uma função para calcular o somatório: 1 + 2/3 + 4/5 + 6/7 + 8/9
+somarFracoes2b - Somar certa quantidade de fracoes recursivamente.
+ @return soma de valores
+ @param x - quantidade de valores a serem mostrados (controle)
+ @param soma - valor atual da soma (historia = memoria)
+ @param numerador - numerador da parcela a ser somada
+ @param denominador - denominador da parcela a ser somada
+*/
+double somarFracoes2b (int x, double soma, double numerador, double denominador)
+{
+	if(x>0)
+	{
+		IO_printf("%d: %lf/%lf\n",x, numerador, denominador);
+		soma= somarFracoes2b (x-1, soma + ((1.0*numerador)/ denominador),
+			numerador +2.0, 
+			denominador+2.0);
+	}
+	return(soma);
+}
+
+/**
+ somarFracoes2a - Somar certa quantidade de fracoes.
+ Funcao de servico para preparar e 
+ disparar o mecanismo recursivo.
+ @return soma de valores
+ @param x - quantidade de valores a serem mostrados
+*/
+double somarFracoes2a(int x)
+{
+	double soma=0.0;
+
+	if(x>0)
+	{
+		IO_printf("%d: %lf\n",x,1.0);
+		soma= somarFracoes2b(x-1, 1.0, 2.0,3.0);
+	}
+	return(soma);
+}
 void method_07 ()
 {
-    
+    double soma= 0.0;
+	IO_id("Method_07 - exemplo0607");
+	soma = somarFracoes2a(5);
+	IO_printf("soma = %lf\n", soma);
+	IO_pause("Apertar ENTER para continuar");
 }
+
+/** 
+saida: 
+5: 1.000000
+4: 2.000000/3.000000
+3: 4.000000/5.000000
+2: 6.000000/7.000000
+1: 8.000000/9.000000
+soma = 4.212698
+*/
+
+/**
+contarDigitos - Contar digitos recursivamente. 
+   @return quantidade de digitos 
+   @param x - numero cuja quantidade de digitos sera' calculada
+*/
+
+int contarDigitos (int x)
+{
+    int resposta =1;
+    if( x>= 10)
+    {
+        resposta = 1 + contarDigitos(x/10); 
+    }
+    else
+    {
+        if(x<0)
+        {
+            resposta = contarDigitos (-x);
+        }
+    }
+    return(resposta);
+}
+
 
 void method_08 ()
 {
-    
-}
+    IO_printf("Method_08 - exemplo0608");
 
+    IO_printf("\nDigitos (%3d) = %d\n", 123, contarDigitos (123));
+    IO_printf("Digitos (%3d) = %d\n", 1, contarDigitos (1));
+    IO_printf("Digitos (%3d) = %d\n", -10, contarDigitos (-10));
+
+    IO_pause("Apertar ENTER para continuar.");
+
+
+}
+/**
+ * Digitos (123) = 3
+   Digitos (  1) = 1
+   Digitos (-10) = 2
+ */
+
+/** 
+   fibonacci - Gerador de numero de Fibonacci. 
+   @return numero de Fibonacci 
+   @param x - numero de ordem cujo valor sera' calculado 
+ */ 
+int fibonacci (int x)
+{
+    int resposta=0;
+    if ( x==1 || x ==2)
+    {
+        resposta =1;
+    }
+    else
+    {
+        if (x>1)
+        {
+            resposta = fibonacci (x-1) + fibonacci (x-2);
+        }
+    }
+    return (resposta);
+}
 void method_09 ()
 {
-    
+
+    IO_printf("Method_09 - exemplo0609");
+
+    IO_printf ( "fibonacci (%d) = %d\n", 1, fibonacci ( 1 ) ); 
+    IO_printf ( "fibonacci (%d) = %d\n", 2, fibonacci ( 2 ) ); 
+    IO_printf ( "fibonacci (%d) = %d\n", 3, fibonacci ( 3 ) ); 
+    IO_printf ( "fibonacci (%d) = %d\n", 4, fibonacci ( 4 ) ); 
+    IO_printf ( "fibonacci (%d) = %d\n", 5, fibonacci ( 5 ) );
+    IO_pause("Apertar ENTER para continuar.");
+
 }
+/** 
+   contarMinusculas - Contador de letras minusculas. 
+   @return quantidade de letras minusculas 
+   @param x - cadeia de caracteres a ser avaliada 
+ */
+
+ int contarMinusculas (chars cadeia, int x)
+ {
+    int resposta=0;
+    if ( 0 <= x && x < strlen(cadeia))
+    {
+        if (cadeia [x] >= 'a'
+        && cadeia[x] <= 'z')
+        {
+            resposta = 1;
+        }
+        resposta = resposta + contarMinusculas (cadeia, x+1);
+    }
+    return (resposta);
+ }
 
 void method_10 ()
 {
-    
+    IO_printf("Method_10 - exemplo0610");
+    IO_printf ( "Minusculas (\"abc\",0)  = %d\n", contarMinusculas ( "abc", 0 ) ); 
+    IO_printf ( "Minusculas (\"aBc\",0)  = %d\n", contarMinusculas ( "aBc", 0 ) ); 
+    IO_printf ( "Minusculas (\"AbC\",0) = %d\n", contarMinusculas ( "AbC", 0 ) ); 
+    IO_pause ( "Apertar ENTER para continuar" );
 }
 
 int main ()
--- ed06/exercicios06.c
+++ ed06/exercicios06.c
@@ -8,3 +8,219 @@
  #include<stdlib.h>
  #include<math.h>
  #include"io.h"
+
+
+ int trescrescente (int n)
+ {
+    int x=3;
+
+    if(n>1)
+    {
+        
+        trescrescente(n-1);
+        IO_printf("%d: %d\n", n, x*n);
+    }
+    else
+    {
+        IO_printf("%d: %d\n",1,x);
+    }
+    
+ }
+
+ void exe0611 ()
+ {
+    int quantidade=0;
+    IO_id("Exercicio0611 - 01");
+    quantidade = IO_readint("Entre com uma quantidade:");
+    trescrescente(quantidade);
+    IO_pause("Apertar ENTER para continuar");
+ }
+ 
+ int tresdecrescente (int n)
+ {
+    int c= 0;
+    int x =3;
+    int y =n;
+    
+
+    for (c=n; c>0; c=c-1)
+    {
+        IO_printf("%d: %d\n", c, x*y);
+        y = y - 1;
+    }
+ }
+
+ 
+ void exe0612 ()
+ {
+    int quantidade=0;
+    IO_id("Exercicio0612 - 02");
+    quantidade = IO_readint("Entre com uma quantidade:");
+    tresdecrescente(quantidade);
+    IO_pause("Apertar ENTER para continuar");
+ }
+
+int fracao1 (int n)
+{
+    int x=3;
+    if(n>1)
+    {
+        fracao1(n-1);
+        IO_printf("1/%d\n",x*(n-1));
+        
+    }   
+    else
+    {
+        IO_printf("1/%d\n",1);
+    }
+}
+
+ void exe0613 ()
+ {
+    int quantidade=0;
+    IO_id("Exercicio0613 - 03");
+    quantidade = IO_readint("Entre com uma quantidade:");
+    fracao1(quantidade);
+    IO_pause("Apertar ENTER para continuar");
+ 
+ }
+
+ int fracao2 (int n)
+ {
+    int x=3;
+    
+    if(n-1>0)
+    {
+    
+        IO_printf("1/%d\n",x*(n-1));
+        fracao2(n-1);
+        
+    }   
+    else
+    {
+        IO_printf("1/%d\n",1);
+    }
+
+ }
+ void exe0614 ()
+ {
+
+    int quantidade=0;
+    IO_id("Exercicio0614 - 04");
+    quantidade = IO_readint("Entre com uma quantidade:");
+    fracao2(quantidade);
+    IO_pause("Apertar ENTER para continuar");
+ }
+
+ int somar (int x)
+ {
+    int soma=0;
+    soma= soma+ x;
+ }
+
+ int numeroNatural (int n)
+ {
+    int x = 3;
+    if (n>1)
+    {
+        x = numeroNatural(x +2*n-1);
+        IO_printf("%d\n",x);
+    }
+ }
+ void exe0615 ()
+ {
+
+ }
+
+ void exe0616 ()
+ {
+
+ }
+
+ void exe0617 ()
+ {
+
+ }
+
+ void exe0618 ()
+ {
+
+ }
+
+ void exe0619 ()
+ {
+
+ }
+
+ void exe0620 ()
+ {
+
+ }
+
+ void exe06E1 ()
+ {
+
+ }
+
+ void exe06E2 ()
+ {
+
+ }
+
+
+ int main ()
+{
+    int op = 0;
+
+    do
+    {
+        IO_id("EXEMPLO ED05 - Izadora Galarza Alves");
+
+        IO_println("Opcoes:");
+        IO_println("0 - Terminar");
+        IO_println("1 - Exercicio 0611");
+        IO_println("2 - Exercicio 0612");
+        IO_println("3 - Exercicio 0613");
+        IO_println("4 - Exercicio 0614");
+        IO_println("5 - Exercicio 0615");
+        IO_println("6 - Exercicio 0616");
+        IO_println("7 - Exercicio 0617");
+        IO_println("8 - Exercicio 0618");
+        IO_println("9 - Exercicio 0619");
+        IO_println("10 - Exercicio 0620");
+        IO_println("11 - Exercicio 06E1");
+        IO_println("12 - Exercicio 06E2");
+
+
+
+
+        op = IO_readint("Escolha um exemplo: ");
+        
+
+        //testar valor 
+        switch(op)
+        {
+            case 0 : break;
+            case 1 : exe0611(); break;
+            case 2 : exe0612();break;
+            case 3 : exe0613(); break;
+            case 4 : exe0614();break;
+            case 5 : exe0615(); break;
+            case 6 : exe0616();break;
+            case 7 : exe0617(); break;
+            case 8 : exe0618();break;
+            case 9 : exe0619(); break;
+            case 10 : exe0620(); break;
+            case 11 : exe06E1(); break;
+            case 12 : exe06E2(); break;
+
+            default:
+                IO_pause ( IO_concat ( "Valor diferente das opcoes (",
+                IO_concat ( IO_toString_d ( op ), ")" ) ) );
+        }
+    } 
+    while( op != 0);
+    IO_pause ( "Apertar ENTER para terminar" );
+return ( 0 );
+
+}
